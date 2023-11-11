d02851d6436be152f891a07d630ac9b1f6ee7c34
Nidia Menchaca <nmenchaca@Nidias-MacBook-Air.local>
2023-11-11 14:37:57 -0600

    writing unit tests

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,28 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId(){
+        Job test1 =new Job();
+        Job test2 =new Job();
+        assertNotEquals(test1.getId(),test2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job test3 = new Job();
+        Job test4 = new Job();
+        assertNotEquals(test3.getId(),test4.getId());
+    }
 }
